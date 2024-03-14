749579b67a4a484619e6e4df9c44f431417dec8f
haidang <haidang12122003@gmail.com>
2024-03-14 15:05:44 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_1/giaiptbac2.dart';
+
+void main() => runApp(MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: HomePage(),
+    ));
